7ac94aa30b4ac09b61e9031fd887818c7f53d7e8
davidtraum <david@traum.me>
2019-11-21 01:15:19 +0100

    Initial

---
+++ assistant.py
@@ -0,0 +1,106 @@
+import time
+import speech_recognition as sr
+import json
+import subprocess
+
+LANG = "de-DE";
+
+DATA = dict();
+PID = dict();
+last_action = 0;
+
+with open('commands.json') as file:
+    DATA = json.loads(file.read());
+
+def log(msg):
+    print("[ASSISTANT] ", msg);
+
+log("Language: " + LANG);
+
+def process(text):
+    global last_action;
+    if(time.time() - last_action < 0.5):
+        return;
+    for key in DATA:
+            entry = DATA[key];
+            doExec = False;
+            if('contains' in entry):
+                for condition in entry['contains']:
+                    full = True;
+                    for word in condition:
+                        if(not word in text):
+                            full = False;
+                            break;
+                    if(full):
+                        doExec = True;
+                        break;
+            if(doExec):
+                execute(entry, text);
+    last_action = time.time();
+                
+def execute(action, text):
+    log("Executing " + action['name'] + "...");
+    do = action['do'];
+    if('append' in action):
+        if(action['append']['mode'] == 'after'):
+            parts = text.split(action['append']['content']);
+            if(len(parts)>1):
+                params = action['append']['content'].join(parts[1:])[1:]
+                log("Parameters: " + params);
+                do += params;
+    if(action['type'] == 'cmd'):
+        try:
+            pid = subprocess.Popen(do.split(' '));
+            log("Executed command: " + do);
+            if('pid' in action):
+                if(action['pid']):
+                    log("Saving Process");
+                    if(action['pid-category'] in PID):
+                        PID[action['pid-category']].append(pid);
+                    else:
+                        PID[action['pid-category']] = [pid];
+        except Exception as e:
+            print(e);
+            log("An error occured while executing shell command.");
+            if('error' in action):
+                log("Running alternative...");
+                execute(DATA[action['error']], text);
+    elif(action['type'] == 'kill'):
+        if action['do'] in PID:
+            for proc in PID[do]:
+                proc.terminate();
+            del PID[action['do']];
+            log("Processes terminated.");
+        else:
+            log("No process found");
+    elif(action['type'] == 'query'):
+        process(action['do']);
+    
+def callback(recognizer, audio):
+    try:
+        text = recognizer.recognize_google(audio, language="de-DE")
+        text = text.lower();
+        log("Understood: " + text);
+        process(text);
+    except sr.UnknownValueError:
+        pass;
+    except sr.RequestError as e:
+        log("Service error");
+        log(e);
+
+r = sr.Recognizer()
+m = sr.Microphone()
+with m as source:
+    r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
+
+# start listening in the background (note that we don't have to do this inside a `with` statement)
+stop_listening = r.listen_in_background(m, callback)
+
+log("Ready");
+try:
+    while True:
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    stop_listening(wait_for_stop=False)
+    log("Exit.");
+    
